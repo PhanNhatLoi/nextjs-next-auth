7ccc65c4fda6caa415b2a80567746fdd500017f9
PhanNhatLoi <phanloi971@gmail.com>
2024-02-21 13:20:34 +0700

    Update route.tsx

--- app/api/auth/test/route.tsx
+++ app/api/auth/test/route.tsx
@@ -1,9 +1,12 @@
 "use server";
 
+import Users from "@/src/models/userModel";
+
 export async function POST(req: Request) {
   return Response.json({ msg: "success" });
 }
 
 export async function GET(req: Request) {
-  return Response.json({ msg: "success" });
+  const users = await Users.find({});
+  return Response.json({ users: users });
 }
