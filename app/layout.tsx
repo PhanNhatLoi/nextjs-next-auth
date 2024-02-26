873db3649227f8de161ce22416cd4b7e3be19c6f
PhanNhatLoi <phanloi971@gmail.com>
2024-02-26 11:03:10 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Sell Shoes",
-  description: "Learn NextJs with sale web",
+  title: "NextJs | Next-auth",
+  description: "Authentication for Next.js",
 };
 
 export default async function RootLayout({
