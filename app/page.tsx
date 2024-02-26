6217af67ca9e7f97cc412e199eb99281bfc63af6
PhanNhatLoi <phanloi971@gmail.com>
2024-02-26 13:48:33 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
-"use client";
+import { auth, signOut } from "@/auth";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
-function HomePage() {
-  return <div>HomePage</div>;
-}
+const SettingPage = async () => {
+  const session = await auth();
+  return (
+    <div>
+      <div>
+        <h1>Welcome to the home page</h1>
+        {!session && (
+          <div>
+            <p>You are not logged in!</p>
+            <Link href={"/login"}>Login</Link>
+          </div>
+        )}
+      </div>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        {session && (
+          <>
+            <h3>Hello, {session.user?.email}</h3>
+            <button type="submit">Logout</button>
+          </>
+        )}
+      </form>
+    </div>
+  );
+};
 
-export default HomePage;
+export default SettingPage;
